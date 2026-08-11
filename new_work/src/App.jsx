f35f91b5dd6c134725ffabf32a23b837f4e4f091
sourav-codes104 <souravsingh.error104@gmail.com>
2026-08-11 23:19:51 +0530

    Counter

--- new_work/src/App.jsx
+++ new_work/src/App.jsx
@@ -1,9 +1,33 @@
-const App=()=>{
-  return(
+import { useState } from "react";
+
+const App = () => {
+  const [counter, setCounter] = useState(0);
+  const increment = () => {
+    if (counter < 20) {
+      setCounter(counter + 1);
+    } else {
+      alert("Counter cannot exceed 20");
+    }
+  };
+  const decrement = () => {
+    if (counter > 0) {
+      setCounter(counter - 1);
+    } else {
+      alert("Counter cannot go beyoond 0");
+    }
+  };
+
+  return (
     <>
-    <h1>Hello World</h1>
+      <h1 style={{ display: "flex", justifyContent: "center" }}>
+        Counter:{counter}
+      </h1>
+      <div style={{display:"flex",justifyContent:"center" ,gap:"30px"}}>
+        <button onClick={increment} style={{height:"30px" ,backgroundColor:"blue", color:"white",border:"2px solid blue",borderRadius:"12px"}}>Increment</button>
+        <button onClick={decrement} style={{height:"30px" ,backgroundColor:"green", color:"white",border:"2px solid green",borderRadius:"12px"}}>Decrement</button>
+      </div>
     </>
   );
-}
+};
 
-export default App
+export default App;
